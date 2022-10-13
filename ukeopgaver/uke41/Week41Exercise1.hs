46c188a83da85b1b8875d27deccd7db84ac5c754
bravo <birk.johannessen@hotmail.com>
2022-10-13 16:33:05 +0200

    from bravo uke41 ass1 not complete but buiilt skeleton

--- ukeopgaver/uke41/Week41Exercise1.hs
+++ ukeopgaver/uke41/Week41Exercise1.hs
@@ -10,7 +10,15 @@ type Graph n = Map n (Set n)
 disjoint :: (Ord a) => Set a -> Set a -> Bool
 disjoint set1 set2 = Set.intersection set1 set2 == Set.empty
 
---hasCycle :: (Ord n) => Graph n -> n -> Bool
---hasCycle g n1 
---  | Map.notMember n1 g = False
---  | disjoint 
+hasCycle :: (Ord n) => Graph n -> n -> Bool
+hasCycle Set.empty n1 = False
+hasCycle g n1 = loop g n1 [n1]
+
+loop :: (Ord n) => Graph n -> n -> [n] -> Bool
+loop g n1 li
+    | Set.member (head li) (Map.lookup n1 g) == n1 = True
+    | null $ filterPrev (Map.lookup n1 g) li       = False
+    | otherwise                                    = False || (loop $ map () (filterPrev (Map.lookup n1 g) li))
+
+filterPrev :: [n] -> Set n -> [n]
+filterPrev prevNodes nodes = filter (\n -> Set.member n prevNodes) nodes
