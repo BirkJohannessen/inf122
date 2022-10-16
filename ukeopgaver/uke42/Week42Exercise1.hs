42012eb64074c49cdbce6b8b0df4ca3316103470
bravo <birk.johannessen@hotmail.com>
2022-10-16 17:42:10 +0200

    push from bravo uke42 in progress

--- ukeopgaver/uke42/Week42Exercise1.hs
+++ ukeopgaver/uke42/Week42Exercise1.hs
@@ -1,5 +1,13 @@
 module Week42Exercise1 where
-
+import Data.Either
 
 --fromLeftAndRight :: (Either a b -> c) -> (a -> c, b -> c)
---fromLeftAndRight
+--fromLeftAndRight (Right b) = uncurry b
+
+
+either' :: (a -> c) -> (b -> c) -> Either a b -> c
+either' _ bc (Right b) = bc b
+either' ac _ (Left a) = ac a
+
+toFstAndSnd :: (a -> (b, c)) -> (a -> b, a -> c)
+toFstAndSnd (a -> (b,c)) = (a -> b, a -> c) 
