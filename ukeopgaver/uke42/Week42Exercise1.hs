e2a95090de2cffb9d724e5616f27d7c744e6209b
bravo <birk.johannessen@hotmail.com>
2022-10-17 16:34:40 +0200

    from bravo, no significant changes.

--- ukeopgaver/uke42/Week42Exercise1.hs
+++ ukeopgaver/uke42/Week42Exercise1.hs
@@ -1,8 +1,9 @@
 module Week42Exercise1 where
-import Data.Either
+import Data.Either 
+
 
 fromLeftAndRight :: (Either a b -> c) -> (a -> c, b -> c)
-fromLeftAndRight f = f . either
+fromLeftAndRight = (\y -> fromLeft',\y -> fromRight')
 
 either' :: (a -> c) -> (b -> c) -> Either a b -> c
 either' _ bc (Right b) = bc b
@@ -13,3 +14,12 @@ toFstAndSnd f = (fst . f, snd . f)
 
 pair :: (a -> b) -> (a -> c) -> a -> (b, c)
 pair f d a = (f a, d a)
+
+
+fromRight' :: Either a b -> b
+fromRight' (Left _)  = error "Data.Either.Combinators.fromRight' encountered a value of form 'Left _', consider using Data.Either.Combinators.fromRight with a default value." -- yuck
+fromRight' (Right x) = x
+
+fromLeft' :: Either a b -> a
+fromLeft' (Right _) = error "Data.Either.Combinators.fromLeft' encountered a value of form 'Right _', consider using Data.Either.Combinators.fromLeft with a default value." -- yuck
+fromLeft' (Left x)  = x
