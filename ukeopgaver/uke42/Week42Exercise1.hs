a0f582b92a0a90f6ded645f279349754816d76ad
BirkJohannessen <Birk.johannessen@hotmail.com>
2022-10-17 23:31:17 +0200

    uke 42 still not complete fuck haskell

--- ukeopgaver/uke42/Week42Exercise1.hs
+++ ukeopgaver/uke42/Week42Exercise1.hs
@@ -1,9 +1,7 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
 module Week42Exercise1 where
-import Data.Either 
-
-
-fromLeftAndRight :: (Either a b -> c) -> (a -> c, b -> c)
-fromLeftAndRight = (\y -> fromLeft',\y -> fromRight')
+import Data.Either
 
 either' :: (a -> c) -> (b -> c) -> Either a b -> c
 either' _ bc (Right b) = bc b
@@ -15,11 +13,7 @@ toFstAndSnd f = (fst . f, snd . f)
 pair :: (a -> b) -> (a -> c) -> a -> (b, c)
 pair f d a = (f a, d a)
 
+fromLeftAndRight :: (Either a b -> c) -> (a -> c, b -> c)
+fromLeftAndRight f = ((.) f fromLeft,(.) f fromRight) 
 
-fromRight' :: Either a b -> b
-fromRight' (Left _)  = error "Data.Either.Combinators.fromRight' encountered a value of form 'Left _', consider using Data.Either.Combinators.fromRight with a default value." -- yuck
-fromRight' (Right x) = x
-
-fromLeft' :: Either a b -> a
-fromLeft' (Right _) = error "Data.Either.Combinators.fromLeft' encountered a value of form 'Right _', consider using Data.Either.Combinators.fromLeft with a default value." -- yuck
-fromLeft' (Left x)  = x
+-- a -> Either a b
