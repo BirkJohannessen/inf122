2ab7794f963607f4bc065faa69132f1dd92249de
bravo <birk.johannessen@hotmail.com>
2022-10-25 02:19:46 +0200

    week43 1 complete from bravo

--- ukeopgaver/uke43/Week43Exercise0.hs
+++ ukeopgaver/uke43/Week43Exercise0.hs
@@ -4,3 +4,11 @@ data BinSearchTree a
   = Empty
   | Branch (BinSearchTree a) a (BinSearchTree a)
   deriving (Eq, Show)
+
+toList :: BinSearchTree a -> [a]
+toList Empty = []
+toList t1 = foldr (:) [] t1
+
+instance Foldable BinSearchTree where
+  foldr _ init Empty = init
+  foldr f ini (Branch t1 r t2) = let b' = f r (foldr f ini t2) in foldr f b' t1
